ab0016303cc743ac0b587aebc1830b7a475fda33
Diego Gomez Mosquera <diegoalejogm@gmail.com>
2017-12-07 19:19:23 -0500

    Added ValueAgent

--- Chapter 2/agents.py
+++ Chapter 2/agents.py
@@ -1,28 +1,35 @@
 import numpy as np
 import abc
 
-class Agent(object):
-    __metaclass__ = abc.ABCMeta
+class ValueAgent(object):
 
-    def __init__(self, environment):
+    def __init__(self, environment, decision_making_policy, action_value_estimation_method=None, initial_values=None, store_rewards=False, alpha=0):
         self.action_space = environment.action_space
-        self.Q = [ 0 for i in range (self.action_space.shape) ]
 
-    @abc.abstractmethod
-    def act(self):
-        pass
+        n = int ( self.action_space.n )
+        self.Q_init = np.zeros(n) if not initial_values else initial_values
+        self.Q = np.copy(self.Q_init)
+        self.N = np.zeros(n)
+        self.R = [ [] for i in range(n) ] if store_rewards else None
+        self.t = 0
+        self.alpha = alpha
 
-    @abc.abstractmethod
+        self.avm = action_value_estimation_method
+        self.dmp = decision_making_policy
+
+        self.last_action = None
+
+    def act(self, update_state=True):
+        action = self.dmp.select_action(self.Q, N=self.N, t=self.t)
+
+        if update_state:
+            self.last_action = action
+            self.N[self.last_action] += 1
+            self.t += 1
+
+        return action
+
     def observe(self, reward):
-        pass
-
-class RandomAgent(Agent):
-
-    def __init__(self, environment):
-        Agent.__init__(self, environment)
-
-    def act(self):
-        return self.action_space.sample()
-
-    def observe(self, reward):
-        pass
+        if self.R is not None:
+            self.R[self.last_action].append(reward)
+        if self.avm: self.avm(self.Q, self.last_action, reward, N=self.N, t=self.t, Q_init=self.Q_init, R=self.R, alpha=self.alpha)
