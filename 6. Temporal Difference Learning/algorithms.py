6f8273c52b9f11f818a9ec53bdcb6b34ade05a12
Diego Gomez Mosquera <diegoalejogm@gmail.com>
2017-12-19 10:37:28 -0500

    Added double Q Learning

--- 6. Temporal Difference Learning/algorithms.py
+++ 6. Temporal Difference Learning/algorithms.py
@@ -81,3 +81,44 @@ def qlearning(env, alpha=0.5, gamma=1, epsilon=0.1, num_episodes=100):
             if done:
                 break
     return Q, sum_rewards
+
+
+def double_qlearning(env, alpha=0.5, gamma=1, epsilon=0.1, num_episodes=100):
+    # Stats tracking
+    sum_rewards = []
+    # Create Q1 and Q2
+    Q1 = zero_value(env)
+    Q2 = zero_value(env)
+    # Run for a given number of times
+    for t in range(num_episodes):
+        sum_rewards.append(0)
+        # Obtain initial state
+        state = env.reset()
+        # Run each episode
+        while True:
+            # Choose action from env given e-greedy policy given Q1 and Q2
+            action = egreedy_policy(state, Q1 + Q2, epsilon)
+            # Take action, obtain next state & reward
+            next_state, reward, done, _ = env.step(action)
+            # Choose policy to update randomly
+            if np.random.rand() < .5:
+                # Choose next action as max Q(S',a) or equivalently max(Q[s'])
+                next_action = np.argmax(Q1[next_state])
+                # Approximate Q
+                Q1[state][action] += alpha * \
+                    (reward + gamma * Q2[next_state]
+                     [next_action] - Q1[state][action])
+            else:
+                # Choose next action as max Q(S',a) or equivalently max(Q[s'])
+                next_action = np.argmax(Q2[next_state])
+                # Approximate Q
+                Q2[state][action] += alpha * \
+                    (reward + gamma * Q1[next_state]
+                     [next_action] - Q2[state][action])
+            # Update state variable
+            state = next_state
+            sum_rewards[t] += reward
+            # Finish episode if done==True
+            if done:
+                break
+    return (Q1 + Q2) / 2, sum_rewards
