44c29980ce70c2f7f6896a02b8979b45ba89f1f0
Diego Gomez Mosquera <diegoalejogm@gmail.com>
2018-02-14 21:55:52 -0500

    Added n-step importance sampling

--- 7.  n-step Bootstrapping/algorithms.py
+++ 7.  n-step Bootstrapping/algorithms.py
@@ -61,3 +61,64 @@ def n_step_sarsa(num_steps, env, num_episodes, policy=None, step_size=.5, discou
                 step += 1
 
     return action_values, episode_rewards
+
+
+def n_step_sarsa_importance_sampling(behaviour_policy, num_steps, env, num_episodes, policy=None, step_size=.5, discount_rate=.9, epsilon=.3):
+
+    episode_rewards = []
+
+    # Init Q(s,a) arbitrarily, for all s in S, a in A(s)
+    action_values = c.ActionValue(u.extract_actions(env))
+    # Init PI to be e-greedy w.r.t. Q, or to a fixed given policy
+    if policy is None:
+        policy = c.EGreedyPolicy(epsilon, action_values)
+
+    # Store and access operation lists
+    states  = c.NstepMemory(num_steps)
+    actions = c.NstepMemory(num_steps)
+    rewards = c.NstepMemory(num_steps)
+
+    for ep in range(num_episodes):
+        # Init and store S0
+        states[0], rewards[0] = env.reset(), 0
+        # Select and store action A0
+        actions[0] = behaviour_policy.sample(states[0])
+        # T: terminal_step = infinity
+        step, terminal_step = 0, float('inf')
+
+        episode_rewards.append(0)
+
+        while True:
+            if step < terminal_step:
+                # Take action a_t. Observe and store Rt+1, St+1
+                states[step+1], rewards[step+1], done, _ = env.step(actions[step])
+                episode_rewards[ep] += rewards[step+1]
+                if done:
+                    terminal_step = step + 1
+                else:
+                    actions[step+1] = behaviour_policy.sample(states[step+1])
+
+            update_step = step - num_steps + 1
+
+            if update_step >= 0:
+                # Calculate n-step return: G
+                G = .0
+                importance_weight = 1
+                for i in range(update_step + 1, min(update_step + num_steps, terminal_step)):
+                    a = policy[states[i], actions[i]]
+                    b = behaviour_policy[states[i], actions[i]]
+                    importance_weight *= policy[states[i], actions[i]] / behaviour_policy[states[i], actions[i]]
+                for i in range(update_step + 1, min(update_step + num_steps+1, terminal_step)):
+                    G += discount_rate ** (i - update_step - 1) * rewards[i]
+                if update_step + num_steps < terminal_step:
+                    G += discount_rate ** (num_steps) * action_values[states[update_step + num_steps], actions[update_step + num_steps]]
+
+                action_values[states[update_step], actions[update_step]] += step_size * importance_weight * (
+                    G - action_values[states[update_step], actions[update_step]])
+
+            if update_step == terminal_step - 1:
+                break
+            else:
+                step += 1
+
+    return action_values, episode_rewards
