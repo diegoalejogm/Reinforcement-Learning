5ff031e5a5785e99239868df062660d52e254302
Diego Gomez <diegoalejogm@users.noreply.github.com>
2018-02-21 22:54:22 -0500

    Added documentation to utils.py.

--- utils.py
+++ utils.py
@@ -4,10 +4,18 @@ import time
 def extract_actions(env):
     return list(range(env.nA))
 
-
-# Method to run environment consecutively!
-# Make sure to run this cell
 def run_environment_greedy(env, Q):
+    '''
+    Executes environment using a greedy policy on the input action value.
+    Environment is ran consecutively and interactively.
+    
+    Parameters
+    ----------
+    env : gym.core.Env
+         OpenAI Gym Environment instance.
+    Q : ActionValue
+        Action Value Instance.
+    '''
     state = env.reset()
     while(True):
         co()
@@ -28,6 +36,17 @@ def run_environment_greedy(env, Q):
     print('Finished!')
 
 def display_episode_log(episode, num_episodes):
+    '''
+    Displays the current execution status, given a total number of episodes
+    and the episode number.
+    
+    Parameters
+    ----------
+    episode : int
+        Current episode
+    num_episodes : int
+        Total number of episodes
+    '''
     clear_output()
     print('Episode: {}/{}'.format(episode, num_episodes))
 
